d2ca68c9e3bbc21b6b963f2dac76b553f13f0ee6
mayankmahavar111 <mayankmahavar111@gmail.com>
2017-10-13 21:57:44 +0530

    initial-2

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from Tkinter import *
 import  os
 import mp3play
 import time
+import tkFileDialog
 
 i=0
 
@@ -83,39 +84,76 @@ def frame():
     root.mainloop()
 
 def playSongs(songs,index):
-    global i
+    global i,f,location
+    try:
+        if f.ispaused() and i==index:
+            f.unpause()
+        else:
+            print "Hello"
+            if index <0 :
+                index=0
+            if index >= len(songs):
+                index=len(songs)-1
+            i=index
+            filename =  location+"/"+str(songs[index])
+            print filename
+            f= mp3play.load(filename)
+            f.play()
+            print f.isplaying(),f.ispaused(),f.volume(100),i
+            print "World"
+    except:
+        if index <0 :
+            index=0
+        if index >= len(songs):
+            index=len(songs)-1
+        i=index
+        filename = location + "/" + str(songs[index])
+        f = mp3play.load(filename)
+        f.play()
+        print f.isplaying(), f.ispaused(), f.volume(100), i
 
-    print "Hello"
-    if index <0 :
-        index=0
-    if index >= len(songs):
-        index=len(songs)-1
-    i=index
-    filename =  "H:\Music\ENGLISH"+"/"+str(songs[index])
-    print filename
-    f= mp3play.load(filename)
-    f.play()
-    print f.isplaying(),f.ispaused(),f.volume(),i
-    print "World"
+def pause(songs,index):
+    global i,f
+    try:
+        if f.isplaying() :
+            f.pause()
+    except:
+        pass
+
 
 def allButton():
-    global i
+    global i,songs,location
+    location="H:\Music\ENGLISH"
     i=0
-    songs = os.listdir("H:\Music\ENGLISH")
+    songs = os.listdir(location)
     root = Tk()
     root.minsize(width=100,height=100)
+    pauseCommand = lambda : pause(songs,i)
     playCommand= lambda : playSongs(songs,i)
     nextCommand = lambda : playSongs(songs,i+1)
     prevCommand = lambda :playSongs(songs,i-1)
+    browseCommand=lambda :fileDialogue()
     play=Tkinter.Button(root,text="Play ",command=playCommand)
     next=Tkinter.Button(root,text="Next ",command=nextCommand)
     prev = Tkinter.Button(root,text="Prev ",command = prevCommand)
+    pau=Tkinter.Button(root,text="Pause",command=pauseCommand)
+    Brow=Tkinter.Button(root,text="Browse",command=browseCommand)
     play.pack()
     next.pack()
     prev.pack()
+    Brow.pack()
+    pau.pack()
     root.mainloop()
 
-
-
+def fileDialogue():
+    global location,songs,i
+    root= Tk()
+    file= tkFileDialog.askdirectory(parent=root,title="Choose a folder")
+    print file
+    location=file
+    songs=os.listdir(location)
+    i=0
+    print location,songs[0]
+    root.destroy()
 
 allButton()
